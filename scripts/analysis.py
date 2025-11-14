7b595d26c2509f09a07cc1932b51d27d023ace35
Dan Nasman <dan.nasman@gmail.com>
2025-11-14 16:54:19 +0200

    update tools

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -27,6 +27,32 @@ def read_data(filename):
 
     return result
 
+def read_recon(filename):
+    result = {}
+
+    f = open(filename, "r")
+
+    s = f.readline()
+    cycle_meas_n = int(s)
+    cycle_avg = 0
+    for i in range(cycle_meas_n):
+        s = f.readline()
+        cycle_avg += int(s)
+    cycle_avg /= cycle_meas_n
+    result["cycle_avg"] = cycle_avg
+
+    s = f.readline()
+    fn_n = int(s)
+    fn = np.zeros(fn_n)
+    for i in range(fn_n):
+        s = f.readline()
+        fn[i] = int(s)
+    result["fn"] = fn
+
+    f.close()
+
+    return result
+
 results = {}
 results["hazard3"] = {}
 results["m33"] = {}
@@ -35,6 +61,7 @@ results["h3_gcc"] = read_data("data/gcc_hazard3.txt")
 results["h3_clang"] = read_data("data/clang_hazard3.txt")
 results["m33_gcc"] = read_data("data/gcc_m33.txt")
 results["m33_clang"] = read_data("data/clang_m33.txt")
+results["reconstructed"] = read_recon("data/reconstructed.txt")
 
 plt.figure()
 
@@ -46,18 +73,27 @@ plt.grid()
 plt.legend()
 plt.tight_layout()
 
+plt.figure()
 
+plt.plot(results["reconstructed"]["fn"])
+plt.grid()
+plt.tight_layout()
+plt.gca().tick_params(axis='y', labelleft=False)
+plt.tight_layout()
+
+plt.savefig("figures/reconstructed_signal.svg", format="svg")
 
 plt.figure()
+div = results["m33_clang"]["cycle_avg"]
 cycle_meas = [
-    results["h3_clang"]["cycle_avg"],
-    results["m33_clang"]["cycle_avg"],
-    results["h3_gcc"]["cycle_avg"],
-    results["m33_gcc"]["cycle_avg"],
+    results["m33_clang"]["cycle_avg"]/div,
+    results["h3_clang"]["cycle_avg"]/div,
+    results["h3_gcc"]["cycle_avg"]/div,
+    results["m33_gcc"]["cycle_avg"]/div,
 ]
 labels = [
-    "Hazard3 (Clang)",
     "ARM Cortex-M33 (Clang)",
+    "Hazard3 (Clang)",
     "Hazard3 (GCC)",
     "ARM Cortex-M33 (GCC)",
 ]
